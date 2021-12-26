301d205beb9ddfece7fe176d4323a98665334c70
HazelJiahe <jw828@cornell.edu>
2021-12-25 22:21:39 -0500

    Edited the coding comments

--- state_string_utils.py
+++ state_string_utils.py
@@ -1,52 +1,40 @@
 import math
-import numpy as np
-from operator import itemgetter
 import netparser
+import numpy as np
 import state_enumerator as se
+from operator import itemgetter
+
+
+'''
+This file holds the code for conversion between state and string. Functions are defined in Class StateStringUtils.
+'''
+
 
 class StateStringUtils:
     ''' Contains all functions dealing with converting nets to net strings
         and net strings to state lists.
     '''
     def __init__(self, state_space_parameters):
-        #self.image_size = state_space_parameters.image_size
         self.output_number = state_space_parameters.output_states
         self.enum = se.StateEnumerator(state_space_parameters)
-    
-    def add_drop_out_states(self, state_list):
-        ''' Add drop out every 2 layers and after each fully connected layer
-        Sets dropout rate to be between 0 and 0.5 at a linear rate
-        '''
-        new_state_list = []
-        number_fc = len([state for state in state_list if state.layer_type == 'fc'])
-        number_gap = len([state for state in state_list if state.layer_type == 'gap'])
-        number_drop_layers = (len(state_list) - number_gap - number_fc)/2 + number_fc
-        drop_number = 1
-        for i in range(len(state_list)):
-            new_state_list.append(state_list[i])
-            if ((((i+1) % 2 == 0 and i != 0) or state_list[i].layer_type == 'fc')
-                and state_list[i].terminate != 1
-                and state_list[i].layer_type != 'gap'
-                and drop_number <= number_drop_layers):
-                drop_state = state_list[i].copy()
-                drop_state.filter_depth = drop_number
-                drop_state.fc_size = number_drop_layers
-                drop_state.layer_type = 'dropout'
-                drop_number += 1
-                new_state_list.append(drop_state)
-
-        return new_state_list
 
     def remove_drop_out_states(self, state_list):
+        '''
+        Remove dropout states from the state_list
+        Parameters: 
+            state_list: list -- A list of states in the network
+        
+        Return:
+            A list of states with drouout states removed.
+        '''
         new_state_list = []
         for state in state_list:
             if state.layer_type != 'dropout':
                 new_state_list.append(state)
         return new_state_list
 
-
     def state_list_to_string(self, state_list):
-        '''Convert the list of strings to a string we can train from according to the grammar'''
+        '''Converts the list of states to a string we can train from according to the grammar.'''
         out_string = ''
         strings = []
         i = 0
@@ -58,16 +46,9 @@ class StateStringUtils:
         return str('[' + ', '.join(strings) + ']')
 
     def state_to_string(self, state):
-        ''' Returns the string asociated with state.
-        '''
+        ''' Returns the string asociated with state.'''
         if state.terminate == 1:
             return 'TERMINATE'
-        # elif state.layer_type == 'conv':
-        #     return 'C(%i,%i,%i)' % (state.filter_depth, state.filter_size, state.stride)
-        # elif state.layer_type == 'gap':
-        #     return 'GAP(%i)' % (self.output_number)
-        # elif state.layer_type == 'pool':
-        #     return 'P(%i,%i)' % (state.filter_size, state.stride)
         elif state.layer_type == 'lstm':
             return 'LSTM(%i,%s)' % (state.units, state.activation)
         elif state.layer_type == 'bilstm':
@@ -85,19 +66,14 @@ class StateStringUtils:
     def convert_model_string_to_states(self, parsed_list, start_state=None):
         '''Takes a parsed model string and returns a recursive list of states.'''
 
-        states = [start_state] if start_state else [se.State('start', 0, 0, None, 0, 0, 0,0, 0)]
+        states = [start_state] if start_state else [se.State('start', 0, 0, None, 0, 0, 0, 0, 0)]
             
         fc_occur = 0
-        
         
         for layer in parsed_list:
             if layer[0] == 'lstm':
                 states.append(se.State(layer_type='lstm',
                                     layer_depth=states[-1].layer_depth + 1,
-                                    # filter_depth=layer[1],
-                                    # filter_size=layer[2],
-                                    # stride=layer[3],
-                                    # image_size=states[-1].image_size,
                                     units=layer[1],
                                     activation=layer[2],
                                     proba=0,
@@ -108,10 +84,6 @@ class StateStringUtils:
             if layer[0] == 'bilstm':
                 states.append(se.State(layer_type='bilstm',
                                     layer_depth=states[-1].layer_depth + 2,
-                                    # filter_depth=layer[1],
-                                    # filter_size=layer[2],
-                                    # stride=layer[3],
-                                    # image_size=states[-1].image_size,
                                     units=layer[1],
                                     activation=layer[2],
                                     proba=0,
@@ -122,10 +94,6 @@ class StateStringUtils:
             elif layer[0] == 'rnn':
                 states.append(se.State(layer_type='rnn',
                                     layer_depth=states[-1].layer_depth + 1,
-                                    # filter_depth=layer[1],
-                                    # filter_size=layer[2],
-                                    # stride=layer[3],
-                                    # image_size=states[-1].image_size,
                                     units=layer[1],
                                     activation=layer[2],
                                     proba=0,
@@ -136,10 +104,6 @@ class StateStringUtils:
             elif layer[0] == 'gru':
                 states.append(se.State(layer_type='gru',
                                     layer_depth=states[-1].layer_depth + 1,
-                                    # filter_depth=layer[1],
-                                    # filter_size=layer[2],
-                                    # stride=layer[3],
-                                    # image_size=states[-1].image_size,
                                     units=layer[1],
                                     activation=layer[2],
                                     proba=0,
@@ -147,32 +111,10 @@ class StateStringUtils:
                                     fc_size=0,
                                     fc_occured = fc_occur,
                                     terminate=0))
-            # elif layer[0] == 'gap':
-            #     states.append(se.State(layer_type='gap',
-            #                             layer_depth=states[-1].layer_depth + 1,
-            #                             filter_depth=0,
-            #                             filter_size=0,
-            #                             stride=0,
-            #                             image_size=1,
-            #                             fc_size=0,
-            #                             terminate=0))
-            # elif layer[0] == 'pool':
-            #     states.append(se.State(layer_type='pool',
-            #                         layer_depth=states[-1].layer_depth + 1,
-            #                         filter_depth=0,
-            #                         filter_size=layer[1],
-            #                         stride=layer[2],
-            #                         image_size=self.enum._calc_new_image_size(states[-1].image_size, layer[1], layer[2]),
-            #                         fc_size=0,
-            #                         terminate=0))
             elif layer[0] == 'fc':
                 fc_occur = 1
                 states.append(se.State(layer_type='fc',
                                     layer_depth=states[-1].layer_depth + 1,
-                                    # filter_depth=len([state for state in states if state.layer_type == 'fc']),
-                                    # filter_size=0,
-                                    # stride=0,
-                                    # image_size=0,
                                     units=0,
                                     activation=layer[2],
                                     proba=0,
@@ -183,10 +125,6 @@ class StateStringUtils:
             elif layer[0] == 'dropout':
                 states.append(se.State(layer_type='dropout',
                                         layer_depth=states[-1].layer_depth + 1,
-                                        # filter_depth=layer[1],
-                                        # filter_size=0,
-                                        # stride=0,
-                                        # image_size=states[-1].image_size,
                                         units=0,
                                     activation=None,
                                     proba=layer[1],
@@ -198,10 +136,6 @@ class StateStringUtils:
                 fc_occur = 1
                 states.append(se.State(layer_type='terminate',
                                     layer_depth=states[-1].layer_depth + 1,
-                                    # filter_depth=len([state for state in states if state.layer_type == 'fc']),
-                                    # filter_size=0,
-                                    # stride=0,
-                                    # image_size=0,
                                     units=0,
                                     activation='linear',
                                     proba=0,
@@ -209,8 +143,34 @@ class StateStringUtils:
                                     fc_size=1,
                                     fc_occured = fc_occur,
                                     terminate=1))
-
         return states
+
+    # The following code is not used in our program.
+    # def add_drop_out_states(self, state_list):
+    #     ''' Add drop out every 2 layers and after each fully connected layer
+    #     Sets dropout rate to be between 0 and 0.5 at a linear rate
+    #     '''
+    #     new_state_list = []
+    #     number_fc = len([state for state in state_list if state.layer_type == 'fc'])
+    #     number_gap = len([state for state in state_list if state.layer_type == 'gap'])
+    #     number_drop_layers = (len(state_list) - number_gap - number_fc)/2 + number_fc
+    #     drop_number = 1
+    #     for i in range(len(state_list)):
+    #         new_state_list.append(state_list[i])
+    #         if ((((i+1) % 2 == 0 and i != 0) or state_list[i].layer_type == 'fc')
+    #             and state_list[i].terminate != 1
+    #             and state_list[i].layer_type != 'gap'
+    #             and drop_number <= number_drop_layers):
+    #             drop_state = state_list[i].copy()
+    #             drop_state.filter_depth = drop_number
+    #             drop_state.fc_size = number_drop_layers
+    #             drop_state.layer_type = 'dropout'
+    #             drop_number += 1
+    #             new_state_list.append(drop_state)
+
+    #     return new_state_list
+
+
 
 
 
