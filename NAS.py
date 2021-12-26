7026718018e12ce1ee3c52657cdf4e02532c0646
Zhuoyang Han <81492838+hzy625@users.noreply.github.com>
2021-12-26 05:51:22 -0500

    Document codes and change generate_new_netork to generate_new_network

--- NAS.py
+++ NAS.py
@@ -5,6 +5,14 @@ import traceback
 import time
 
 class NAS():
+    '''Function generate_new_network generates new networks and returns the network as string and Q training step as integer.
+       Function incorporate_trained_net adds given network to replay database and store for future use.
+
+       Args:
+            list_path: Path of folder that stores replay dictionary and Q value csv files
+            state_space_parameters: Parameters of state space
+            hyper_parameters: Hyperparameters       
+    '''
     def __init__(self,
                  list_path,
                  state_space_parameters,
@@ -12,7 +20,7 @@ class NAS():
                  epsilon=None,
                  number_models=None):
 
-        self.replay_columns = ['net',                   #Net String
+        self.replay_columns = ['net',
                                'accuracy_best_val',
                                'epsilon',
                                'iteration']         
@@ -41,6 +49,7 @@ class NAS():
 
 
     def load_replay(self):
+        '''Loads replay database from path, or creates one if file does not exist'''
         if os.path.isfile(self.replay_dictionary_path):
             print ('Found replay dictionary')
             replay_dic = pd.read_csv(self.replay_dictionary_path)
@@ -51,7 +60,7 @@ class NAS():
         return replay_dic, q_training_step
 
     def load_qlearner(self):
-        # Load previous q_values
+        '''Loads Q values if it exists and then loads Q learner'''
         if os.path.isfile(os.path.join(self.list_path, 'q_values.csv')):
             print ('Found q values')
             qstore = q_learner.QValues()
@@ -68,26 +77,25 @@ class NAS():
         return ql
 
     def filter_replay_for_first_run(self, replay):
-        ''' Order replay by iteration, then remove duplicate nets keeping the first'''
+        '''Sorts replay by iteration, then removes duplicate networks while keeping the first one'''
         temp = replay.sort_values(['iteration']).reset_index(drop=True).copy()
         return temp.drop_duplicates(['net'])
 
     def number_trained_unique(self, epsilon=None):
-        '''Epsilon defaults to the minimum'''
+        '''Returns the total number of unique networks'''
         replay_unique = self.filter_replay_for_first_run(self.replay_dictionary)
         eps = epsilon if epsilon else min(replay_unique.epsilon.values)
         replay_unique = replay_unique[replay_unique.epsilon == eps]
         return len(replay_unique)
 
     def check_reached_limit(self):
-        ''' Returns True if the experiment is complete
-        '''
+        ''' Returns True if the experiment is complete'''
         if len(self.replay_dictionary):
             completed_current = self.number_trained_unique(self.epsilon) >= self.number_models
 
             if completed_current:
                 if self.schedule_or_single:
-                    # Loop through epsilon schedule, If we find an epsilon that isn't trained, start using that.
+                    # Loops through epsilon schedule, and updates the epsilon to the one not trained
                     completed_experiment = True
                     for epsilon, num_models in self.state_space_parameters.epsilon_schedule:
                         if self.number_trained_unique(epsilon) < num_models:
@@ -106,19 +114,20 @@ class NAS():
             else:
                 return False
 
-    def generate_new_netork(self):
+    def generate_new_network(self):
+        '''Generates a new network and update Q training step, return the network as string and step as integer'''
         try:
             (net,
              acc_best_val) = self.qlearner.generate_net()
 
-            # We have already trained this net
+            # Check if we have already trained this net
             if net in self.replay_dictionary.net.values:
                 self.q_training_step += 1
                 self.incorporate_trained_net(net,
                                              acc_best_val,
                                              self.epsilon, 
                                              self.q_training_step)
-                return self.generate_new_netork()
+                return self.generate_new_network()
             else:
                 self.q_training_step += 1
                 return net, self.q_training_step
@@ -131,9 +140,8 @@ class NAS():
                                 acc_best_val,                         
                                 epsilon,
                                 iteration):
-
+        '''Adds given trained network to the replay database and update local file'''
         try:
-            # If we sampled the same net many times, we should add them each into the replay database
             self.replay_dictionary = pd.concat([self.replay_dictionary, pd.DataFrame({'net':[net_string],
                                                                                           'accuracy_best_val':[acc_best_val],
                                                                                           'epsilon': [epsilon],
